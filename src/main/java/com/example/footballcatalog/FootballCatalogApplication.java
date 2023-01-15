7d995b132826799b41e2a20fc4877e38d50b5f72
KirillDmit <kirilldmitriev20015@gmail.com>
2023-01-16 00:34:27 +0500

    Initial commit

---
+++ src/main/java/com/example/footballcatalog/FootballCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/footballcatalog/FootballCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
